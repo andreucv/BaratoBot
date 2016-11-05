2751d9555a4f32bed55e8fa6d285f56f408bbaf3
andreucv <EricaMultiflora2014>
2016-11-05 18:41:05 +0100

    Changed method to echo_all

--- runBot.py
+++ runBot.py
@@ -4,12 +4,16 @@ import analyser
 token = '250303321:AAEpOPI1RGL_md0ZaLvVvYynDAleeSCXivM'
 bot = telebot.TeleBot(token)
 
-def listener(messages):
-    for message in messages:
-        response, keyboard = analyse(message)
-        bot.send_message(message.chat_id, response)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
-bot.set_update_listener(listener)
+#def listener(messages):
+#    for message in messages:
+#        response, keyboard = analyse(message)
+#        bot.send_message(message.chat_id, response)
+
+#bot.set_update_listener(listener)
 bot.polling()
 
 while True: pass
