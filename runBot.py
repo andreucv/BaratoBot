fed18d019a57da0416c9c41a042eb8077c9fc86d
andreucv <EricaMultiflora2014>
2016-11-05 18:44:23 +0100

    cleaning code

--- runBot.py
+++ runBot.py
@@ -6,14 +6,8 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(message.chat_id, message.text)
 
-#def listener(messages):
-#    for message in messages:
-#        response, keyboard = analyse(message)
-#        bot.send_message(message.chat_id, response)
-
-#bot.set_update_listener(listener)
 bot.polling()
 
 while True: pass
